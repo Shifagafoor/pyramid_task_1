e238611d9fb8fba27b3e0ca4835f0a2dba17c897
Shifa Gafoor A <shifagafoorncas@gmail.com>
2025-01-10 07:57:22 +0530

    .

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let rows = 5;
-for(i=1; i<rows; i++) {
-    for(let j=1; j<=rows - j; j++){
-        document.write("&nbsp;&nbsp;");
-    }
+// let rows = 5;
+// for(i=1; i<rows; i++) {
+//     for(let j=1; j<=rows - j; j++){
+//         document.write("&nbsp;&nbsp;");
+//     }
 
-    for(z=1; z<=2; z++){
-        document.write("*")
-    }
+//     for(z=1; z<=2; z++){
+//         document.write("*")
+//     }
     
-}
+// }
