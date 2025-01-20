afea52c854858626dcebfae2184a72146d78310a
Shifa Gafoor A <shifagafoorncas@gmail.com>
2025-01-20 10:37:16 +0530

    .

--- script.js
+++ script.js
@@ -1,96 +1,3 @@
-// const draw_button = document.querySelector('.draw');
-// const display_output = document.querySelector('.display_output');
-// const start_button = document.querySelector('.start');
-// const stop_button = document.querySelector('.stop');
-// const restart_button = document.querySelector('.restart');
-// const colors = document.querySelector('.color');
-
-// let delay;
-// let selectedColor = null;
-
-// colors.addEventListener('input', (event) => {
-//     selectedColor = event.target.value;
-//     console.log(selectedColor);
-// });
-
-// let isAnimating = false;
-
-// draw_button.addEventListener('click', () => {      
-//     const numRows = parseInt(document.querySelector('.pyramid .input').value);
-
-//     display_output.innerHTML = '';
-//     for (let i = 1; i <= numRows; i++) {
-//         const rowDiv = document.createElement('div');
-//         rowDiv.style.display = 'flex';
-//         rowDiv.style.justifyContent = 'center';
-//         rowDiv.setAttribute("class", "rowDiv")
-
-//         for (let j = 0; j < i; j++) {
-//             const circle = document.createElement('div');
-//             circle.style.width = '20px';
-//             circle.style.height = '20px';
-//             circle.style.border = '2px solid white';
-//             circle.style.borderRadius = '50%';
-//             circle.style.margin = '5px';
-//             circle.setAttribute("class", "circle")
-
-//             rowDiv.appendChild(circle);
-
-//         }
-
-//         display_output.appendChild(rowDiv);
-//     }
-// });
-
-// let animationTimeouts = [];
-
-// start_button.addEventListener('click', () => {
-//     start_button.disabled = true;
-//     stop_button.disabled = false;
-
-//     const rows = display_output.querySelectorAll('.rowDiv');
-//     delay = document.querySelector('.delay-input  .input').value;
-
-//     rows.forEach((row, index) => {
-        
-//         const timeout = setInterval(() => {
-//             row.style.opacity = 1;
-//             // console.log(row)
-//             const timeout = setTimeout(() => {
-//                 row.classList.add("active")
-            
-            
-//                 setTimeout(() => {
-//                     row.classList.remove("active")
-//                 },  delay);
-            
-            
-//             },index*delay);
-//             animationTimeouts.push(timeout);
-//         }, rows.length*delay);
-//         animationTimeouts.push(timeout);
-//     });
-
- 
-// });
-
-// stop_button.addEventListener('click', () => {
-//     start_button.disabled = false;
-//     stop_button.disabled = true;
-
-//     animationTimeouts.forEach(timeout => clearTimeout(timeout));
-//     animationTimeouts = [];
-// });
-
-// restart_button.addEventListener('click', () => {
-//     start_button.disabled = false;
-//     stop_button.disabled = true;
-
-//     display_output.innerHTML = '';
-//     animationTimeouts = [];
-// });
-
-
 const draw_button = document.querySelector(".draw");
 const display_output = document.querySelector(".display_output");
 const start_button = document.querySelector(".start");
@@ -146,6 +53,7 @@ start_button.addEventListener("click", () => {
     }, (i - lastActiveIndex) * delay);
 
     const timeout = setTimeout(() => {
+        row.style.opacity = 1;
       row.classList.remove("active");
     }, (i - lastActiveIndex) * delay + delay);
 
